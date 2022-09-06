aa95fa13d509a73d87305d317aaba7ec5f740804
WolfsParadise5 <triggered5222@gmail.com>
2022-09-07 02:40:40 +0800

    added functions and initializer

--- src/class/property.rb
+++ src/class/property.rb
@@ -1,4 +1,53 @@
 require 'space'
 
 class Property < Space
+
+    def initialize(name, price, housePrice, hotelPrice, rentAmount, rentHouseAmount, rentHotelAmount)
+        super(name)
+        @owner = "Bank"
+        @PRICE = price
+        @HOUSE_PRICE = housePrice
+        @HOTEL_PRICE = hotelPrice
+        @house_amount = 0
+        @hotel_amount = 0
+        @RENT_AMOUNT = rentAmount
+        @RENT_HOUSE_AMOUNT = rentHouseAmount
+        @RENT_HOTEL_AMOUNT = rentHotelAmount
+    end
+
+    def price
+        @PRICE
+    end
+
+    def housePrice
+        @HOUSE_PRICE
+    end
+
+    def hotelPrice
+        @HOTEL_PRICE
+    end
+
+    def houseAmount
+        @house_amount
+    end
+
+    def hotelAmount
+        @hotel_amount
+
+    def rentAmount
+        @RENT_AMOUNT
+    end
+
+    def rentHouseAmount
+        @RENT_HOUSE_AMOUNT
+    end
+
+    def rentHotelAmount
+        @RENT_HOTEL_AMOUNT
+    end
+
+    def calcRent
+        return @RENT_AMOUNT + (@house_amount * @RENT_HOUSE_AMOUNT) + (@hotel_amount * @RENT_HOTEL_AMOUNT)
+    end       
+
 end
