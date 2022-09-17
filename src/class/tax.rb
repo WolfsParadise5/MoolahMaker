8307bce079781d75a168f23077d8507474e388d6
WolfsParadise5 <triggered5222@gmail.com>
2022-09-18 03:20:06 +0800

    Added methods to tax class

--- src/class/tax.rb
+++ src/class/tax.rb
@@ -1,4 +1,18 @@
 require 'space'
 
 class Tax < Space
+
+    def initialize(taxPercentWallet,taxPercentProp,taxPercentHouse,taxPercentSkyscraper)
+        @taxPercentWallet = taxPercentWallet
+        @taxPercentProp = taxPercentProp
+        @taxPercentHouse = taxPercentHouse
+        @taxPercentSkyscraper = taxPercentSkyscraper
+    end
+
+    def calcTax(walletValue, totalHouse, houseVal, totalSkyscraper, skyscraperVal, totalPropVal)
+        @totalWallet = walletValue * taxPercentWallet
+        @totalHouse = houseVal * totalHouse * taxPercentHouse
+        @totalSkyscraper = skyscraperVal * totalSkyscraper * taxPercentSkyscraper
+        return @totalWallet + @totalHouse + @totalSkyscraper
+    end
 end
