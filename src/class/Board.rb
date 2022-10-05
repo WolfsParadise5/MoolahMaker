7dd0844f4c7c152b656da1914e6b56f0e0e82224
Haqeem Wan <haqeemwan@gmail.com>
2022-10-05 13:08:32 +0800

    Added Board and Player Class

---
+++ src/class/Board.rb
@@ -0,0 +1,18 @@
+class Board
+    attr_accessor :space, :turns
+
+    def initialize(space, turns)
+        @space = 65
+        @turns = 0
+    end
+
+    def changeCycle()
+    end
+
+    def changePlayerPos()
+    end
+
+    def checkPanel()
+    end
+
+end
---
+++ src/class/Player.rb
@@ -0,0 +1,14 @@
+class Rebate
+    attr_accessor :name, :position, :balance, :netWorth, :rebateValue, :isStigma, :reputation, :vacationCounter
+
+    def initialize(space, turns)
+        @name = 
+        @position = 
+        @balance = 
+        @netWorth = 
+        @rebateValue = 
+        @isStigma = 
+        @reputation = 
+        @vacationCounter =
+    end
+end
